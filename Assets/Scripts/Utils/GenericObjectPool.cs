581e1658f6de17b4fa94b8bdf8faaa3e804f0130
Cristian <cri.ghiorzi@gmail.com>
2023-10-01 09:53:12 +0200

    Project created

---
+++ Assets/Scripts/Utils/GenericObjectPool.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class GenericObjectPool<T> : MonoBehaviour where T : Component
+{
+    [SerializeField] private T prefab;
+
+    public static GenericObjectPool<T> Instance { get; private set; } // Singleton instance
+    private Queue<T> objects = new Queue<T>();
+
+    private void Awake()
+    {
+        // Singleton Instance
+        Instance = this;
+    }
+
+    public T Get()
+    {
+        if(objects.Count == 0)
+        {
+            AddObject(1);
+        }
+        return objects.Dequeue();
+    }
+
+    public void ReturnToPool(T objectToReturn)
+    {
+        objectToReturn.gameObject.SetActive(false);
+        objects.Enqueue(objectToReturn);
+    }
+
+    private void AddObject(int count) {
+        var newObject = GameObject.Instantiate(prefab);
+        newObject.gameObject.SetActive(false);
+        objects.Enqueue(newObject);
+    }
+ }
---
+++ Assets/Scripts/Utils/Singleton.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T :Component{
+    public static T Instance { get; private set; }
+    protected virtual void Awake() {
+
+        if (Instance != null && Instance != this) {
+            Destroy(this);
+        }
+        else {
+            Instance = this as T;
+        }
+    }
+}
